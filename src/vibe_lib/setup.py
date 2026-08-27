11eb4d82e8f41acec0e57a0febec6f0a6189994e
Renato Luiz de Freitas Cunha <renatocunha@acm.org>
2026-08-27 00:01:03 -0300

    Update shared Python dependencies

--- src/vibe_lib/setup.py
+++ src/vibe_lib/setup.py
@@ -13,5 +13,11 @@ setup(
     keywords="terravibes geospatial",
     packages=find_packages(exclude=["tests*"]),
     python_requires="~=3.8",
-    install_requires=["numpy", "geopandas", "rasterio~=1.2"],
+    install_requires=[
+        "numpy",
+        "geopandas",
+        "planetary-computer~=1.0.0",
+        "pystac-client~=0.3.2",
+        "rasterio~=1.2",
+    ],
 )
